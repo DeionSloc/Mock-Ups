1713f5eb32cf880ae7101550e7b90ba3f2c9ce0e
DeionSloc <95542183+DeionSloc@users.noreply.github.com>
2025-09-29 20:54:14 -0600

    Create project folder
    Created 'Business Mock-ups' project folder and pushed to github

---
+++ Business Mock-ups/Business Mock-ups/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Business_Mock_ups.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
